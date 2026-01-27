efb4dffb149fcff319f9f25ab048466bb90a0b62
Sudarsan077 <mssusan077@gmail.com>
2026-01-27 21:03:09 +0530

    Added objects.js file

---
+++ objects.js
@@ -0,0 +1,9 @@
+var person = {};
+
+person["FirstName"] = "Sudarsan";
+person["LastName"] = "Maruthamuthu";
+person.age = 24;
+person.designation = "Web developer";
+person.address = "Chennai, india"
+
+console.log(person.FirstName + " Was working as " + person.designation)
